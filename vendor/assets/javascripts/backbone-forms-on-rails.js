ac70eb525233ad8f554b247fbd83e6b5a2acef75
John Antoni Griffiths <john@uxgent.co>
2013-02-02 22:48:00 -0500

    Update vendor/assets/javascripts/backbone-forms-on-rails.js

--- vendor/assets/javascripts/backbone-forms-on-rails.js
+++ vendor/assets/javascripts/backbone-forms-on-rails.js
@@ -1,3 +1,5 @@
 //= require backbone-forms-on-rails/backbone-forms.js
 //= require backbone-forms-on-rails/templates/bootstrap.js
 //= require backbone-forms-on-rails/adapters/backbone.bootstrap-modal.js
+//= require backbone-forms-on-rails/backbone-validation.js
+
